6780a3b7807635d1f943aceb6561c2eab79acd61
scott coates <scoarescoare@gmail.com>
2016-12-10 14:15:25 -0600

    updated deps

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Router, Route, browserHistory } from 'react-router';
+
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux'
 import { reducer as formReducer } from 'redux-form'
 
 require('smoothscroll-polyfill').polyfill();
-
 
 import App from './scripts/app';
 import './styles/index.css';
@@ -19,7 +20,9 @@ const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}/>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );

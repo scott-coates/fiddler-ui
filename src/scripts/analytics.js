59988c1a5900310028bc11a115beb16fc820fc44
scott coates <scoarescoare@gmail.com>
2016-12-12 17:44:16 -0600

    new key

--- src/scripts/analytics.js
+++ src/scripts/analytics.js
@@ -1,6 +1,6 @@
 import Analytics from 'analytics.js-loader';
 
-const key       = process.env.NODE_ENV === 'production' ? 'ufEPB1onsr4oPKSGJgfHHgoiTglC9S4Q' : 'X1bRi9fPNRIuCk3ZfYEtMZefYSoBzNtH';
+const key       = process.env.NODE_ENV === 'production' ? '2wZrWzavkd5UqYWdxN1IN7aZKMCNl70C' : 'X1bRi9fPNRIuCk3ZfYEtMZefYSoBzNtH';
 const analytics = new Analytics({writeKey: key});
 
 export default analytics;
